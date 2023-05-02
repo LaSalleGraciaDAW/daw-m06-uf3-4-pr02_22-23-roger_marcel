f5a647ec0183f51a075fadf03cd0a520489b7de3
MarcelLaSalle <115008364+MarcelLaSalle@users.noreply.github.com>
2023-05-02 16:16:36 +0200

    formulari ciutats

--- src/pages/CityList/index.jsx
+++ src/pages/CityList/index.jsx
@@ -1,11 +1,38 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 function CityList({countrySelected, setCountrySelected}) {
     const {countryId} = useParams();
 
+    const fetchedCities = useFetch("http://localhost:3000/db/cities.json");
+
+    const [cities, setCities] = useState(...fetchedCities);
+    const [searchValue, setSearchValue] = useState();
+
+
+    useEffect(() => {    
+        setCities((prevCities) => prevCities.filter(city => city.country_id === countryId));
+    }, [countryId]);
+    
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (searchValue) {
+                setCities((prevCities) => prevCities.filter(cities => cities.nombre.includes(searchValue)))
+            }
+        }, 500);
+
+        return () => {
+            console.log('Effect cleanup!');
+            clearTimeout(timer);
+        };
+    }, [searchValue]);
+
         return (
             <div>
                 <h2>Search cities in {countrySelected}</h2>
+                Buscar Ciutat: <input type="text" onChange={(e) => setSearchValue(e.target.value)}></input>
             </div>
         );
 }
